1992a0068973bcbde3977cb582b5ad44310408bc
melou-dev <ameliecuaces.dev@gmail.com>
2019-11-14 10:37:04 +0100

    plug edit and delete button into todolist-node.js

--- app.js
+++ app.js
@@ -52,8 +52,8 @@ app.get("/", (req, res) => {
                 <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                   <span class="item-text">${item.dataValues.item}</span>
                   <div>
-                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                    <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                    <button data-id="${item.dataValues.id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button data-id="${item.dataValues.id}" class="delete-me btn btn-danger btn-sm">Delete</button>
                   </div>
                 </li>`;
             })
@@ -61,6 +61,7 @@ app.get("/", (req, res) => {
         </ul>
         
       </div>
+
       <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
       <script src="/js/edit.js"></script>
       </body>
@@ -81,8 +82,22 @@ app.post("/", (req, res) => {
 });
 
 app.post("/update-item", (req, res) => {
-  console.log(req.body.itemUpdated);
-  res.send("Success");
+  Item.update(
+    { item: req.body.itemUpdated },
+    {
+      where: {
+        id: req.body.id
+      }
+    }
+  );
+});
+
+app.post("/delete-item", (req, res) => {
+  Item.destroy({
+    where: {
+      id: req.body.id
+    }
+  });
 });
 
 app.listen(port);
--- public/js/edit.js
+++ public/js/edit.js
@@ -1,11 +1,28 @@
-  document.addEventListener("click", function(e) {
-    if (e.target.classList.contains("edit-me")) {
-      let userInput = prompt("Veuillez entrer votre nouvelle tâche ...");
+document.addEventListener("click", function(e) {
+  if (e.target.classList.contains ("delete-me")) {
+    if(confirm("Do you really want delete this item?"))
+    {
       axios
-        .post("/update-item", { itemUpdated: userInput })
-        .then()
-        .catch(err => {
-          console.log(err);
-        });
+      .post("/delete-item", {
+        id: e.target.getAttribute("data-id")
+      })
+      .then()
+      .catch(err => {
+        console.log(err);
+      });
     }
-  });
+  }
+
+  if (e.target.classList.contains("edit-me")) {
+    let userInput = prompt("Veuillez entrer votre nouvelle tâche ...");
+    axios
+      .post("/update-item", {
+        itemUpdated: userInput,
+        id: e.target.getAttribute("data-id")
+      })
+      .then()
+      .catch(err => {
+        console.log(err);
+      });
+  }
+});
